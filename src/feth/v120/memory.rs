5eebb1b5a286686b290231c8dfe4144f248c5c65
Andy Dautricourt <raytwost@gmail.com>
2020-05-16 00:07:09 +0200

    Fix some use statements

--- src/feth/v120/memory.rs
+++ src/feth/v120/memory.rs
@@ -1,16 +1,17 @@
-use skyline::libc;
+use skyline::from_offset;
+use skyline::libc::c_void;
 
-#[skyline::from_offset(0x540b30)]
-pub fn kt_malloc(size: u32) -> *const skyline::libc::c_void;
+#[from_offset(0x540b30)]
+fn kt_malloc(size: u32) -> *const c_void;
 
-#[skyline::from_offset(0x5BAB80)]
-pub fn kt_aligned_malloc(size: libc::size_t, align: u64) -> *const libc::c_void;
+#[from_offset(0x5BAB80)]
+fn kt_aligned_malloc(size: u64, align: u64) -> *const c_void;
 
-#[skyline::from_offset(0x540b50)]
-pub fn kt_free(pointer: *mut skyline::libc::c_void) -> *const skyline::libc::c_void;
+#[from_offset(0x540b50)]
+fn kt_free(pointer: *mut c_void) -> *const c_void;
 
-#[skyline::from_offset(0x540b70)]
-pub fn kt_realloc(pointer: *mut skyline::libc::c_void, size: libc::size_t) -> *const skyline::libc::c_void;
+#[from_offset(0x540b70)]
+fn kt_realloc(pointer: *mut c_void, size: u64) -> *const c_void;
 
-#[skyline::from_offset(0x540b90)]
-pub fn kt_calloc(t1: u32, t2: u32) -> *const skyline::libc::c_void;
+#[from_offset(0x540b90)]
+fn kt_calloc(t1: u32, t2: u32) -> *const c_void;
--- src/feth/v120/save.rs
+++ src/feth/v120/save.rs
@@ -1,10 +1,11 @@
-#![feature(proc_macro_hygiene)]
-use skyline::{hook, install_hook};
+use skyline::from_offset;
 
+// TODO
 #[repr(C)]
-pub struct Character {
-    
-}
+struct Character {}
 
-#[skyline::from_offset(0x3CAF30)]
-pub fn find_character_by_id(save_structure: *const skyline::libc::c_void, character_id: u32) -> *mut skyline::libc::c_void;
+#[from_offset(0x3CAF30)]
+fn find_character_by_id(
+    save_structure: *const skyline::libc::c_void,
+    character_id: u32,
+) -> *mut skyline::libc::c_void;
