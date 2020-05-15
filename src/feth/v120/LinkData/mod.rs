0d354576a02b49ddfd6c6f57dfcf00456f878517
Andy Dautricourt <raytwost@gmail.com>
2020-05-16 01:34:15 +0200

    add info_get_filename to linkdata module

--- src/feth/v120/LinkData/mod.rs
+++ src/feth/v120/LinkData/mod.rs
@@ -1,8 +1,11 @@
 use skyline::from_offset;
-use skyline::libc::c_void;
+use skyline::libc::{ c_void, c_char };
 
 #[from_offset(0x4A4700)]
 pub fn get_uncompressed_size_data(file_id: u32) -> u64;
 
 #[from_offset(0x4A0B40)]
 pub fn get_uncompressed_size(linkdata_mgr: *const c_void, file_id: u32) -> u64;
+
+#[from_offset(0x4A47D0)]
+pub fn info_get_filepath(out_path: c_char, file_id: u32);
