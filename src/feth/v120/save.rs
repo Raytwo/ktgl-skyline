1b6aeb8e788ece8287ef924769d5513fa5ee3883
Raytwo <raytwost@gmail.com>
2020-05-19 00:57:22 +0200

    Populate test structure for macro testing

--- src/feth/v120/save.rs
+++ src/feth/v120/save.rs
@@ -2,10 +2,12 @@ use skyline::from_offset;
 
 // TODO
 #[repr(C)]
-struct Character {}
+pub struct Character {
+    test: u32,
+}
 
 #[from_offset(0x3CAF30)]
-pub fn find_character_by_id(
+fn find_character_by_id(
     save_structure: *const skyline::libc::c_void,
     character_id: u32,
 ) -> *mut skyline::libc::c_void;
