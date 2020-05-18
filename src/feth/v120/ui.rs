5aa413a851722953189604fb82a7a40cdbe60009
Raytwo <raytwost@gmail.com>
2020-05-18 21:29:42 +0200

    Ditto

--- src/feth/v120/ui.rs
+++ src/feth/v120/ui.rs
@@ -8,4 +8,4 @@ pub fn show_popup_window(message: *const c_char, sfx_id: u32, unk3: u64, unk4: u
 pub fn get_message_by_id(message_id: u32) -> *const c_char;
 
 #[from_offset(0x5C5E40)]
-pub fn display_text(pos_x: f32, pos_y: f32, scale_x: f32, scale_y: f32, text: *const c_char, box_clipping: u32, unk1: u32, centered: bool, unk2: i32, unk3: i32, shading_effect: i8, alpha: i8);
+pub fn display_text(pos_x: f32, pos_y: f32, scale_x: f32, scale_y: f32, text: *const c_char, box_clipping: u32, unk1: u32, centered: bool, unk2: i32, unk3: i32, shading_effect: u8, alpha: i8);
